f7e4f7cb92ed18e18bc8fd0ca79aae37bf7a6e53
Walexxyy1 <dewaleadesope@gmail.com>
2024-02-19 12:24:59 +0100

    first commit

--- RedisDemo/obj/Debug/net7.0/RedisDemo.AssemblyInfo.cs
+++ RedisDemo/obj/Debug/net7.0/RedisDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedisDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dac67bb5140351b78620dbf9cd7ddfc6d86f623")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedisDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedisDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
